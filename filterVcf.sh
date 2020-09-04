360120583da23096dbb60eff12557825a3570820
Daniel <dtiezzi@usp.br>
2020-09-04 19:18:53 -0300

    'full mutect2 script'

--- filterVcf.sh
+++ filterVcf.sh
@@ -6,9 +6,9 @@ REFGENOME='/mnt/md0/humanGenome/TCGA/GRCh38.d1.vd1.fa'
 OUTDIR='mutcallFiltered'
 FUNCOTATODATA='/home/dtiezzi/Softwares/gatk-4.1.6.0/funcotator_dataSources.v1.6.20190124s/'
 
-/home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" LearnReadOrientationModel -I fir2/f1r2.tar.gz -O ${name}_read-orientation-model.tar.gz
+/home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" LearnReadOrientationModel -I f1r2/f1r2.tar.gz -O ${name}_read-orientation-model.tar.gz
 
-/home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" \ FilterMutectCalls -V $INPUTDIR/${name}.targeted_sequencing.tumor_only.gatk4_mutect2.raw_somatic_mutation.vcf.gz -R $REFGENOME --ob-priors ${name}_read-orientation-model.tar.gz --min-allele-fraction 0.05 --unique-alt-read-count 10 -O $OUTDIR/${name}_somatic_filtered.vcf
+/home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" FilterMutectCalls -V $INPUTDIR/${name}.targeted_sequencing.tumor_only.gatk4_mutect2.raw_somatic_mutation.vcf.gz -R $REFGENOME --ob-priors ${name}_read-orientation-model.tar.gz --min-allele-fraction 0.05 --unique-alt-read-count 10 -O $OUTDIR/${name}_somatic_filtered.vcf
 
 grep -E "#|PASS" $OUTDIR/${name}_somatic_filtered.vcf > $OUTDIR/${name}_somatic_filtered_PASS.vcf
 
--- mutect2full.sh
+++ mutect2full.sh
@@ -5,9 +5,12 @@ while read -r line
 
     ## GLOBAL VARIABLES
 
+    INPUTBAM='outputBam'
+    MIDDIR='sortedBam'
+    OUTPUTDIR='mkDupGatk'
     GENOME='./refGenome/GRCh38_filt.fa'
     REFDICT='./refGenome/GRCh38_filt.dict'
-    INPUTBAM='dupBam'
+    #INPUTBAM='dupBam'
     GNOMAD='/mnt/md0/somatic-hg38-af-only-gnomad.hg38.vcf.gz'
     BEDINTERVALS='/mnt/md0/humanGenome/TCGA/intervals.bed'
     REFGENOME='/mnt/md0/humanGenome/TCGA/GRCh38.d1.vd1.fa'
@@ -15,6 +18,43 @@ while read -r line
     PONFILE='/home/dtiezzi/ncbi/dbGaP-19480/mutect/gatk4_mutect2_4136_pon.vcf.gz'
     OUTVCFS='vcfDir'
     VCFSORTED='vcfSorted'
+    INPUTDIR='vcfFinal'
+    OUTDIR='mutcallFiltered'
+    FUNCOTATODATA='/home/dtiezzi/Softwares/gatk-4.1.6.0/funcotator_dataSources.v1.6.20190124s/'
+
+    echo '[INFO] sorting ' $SAMPLE ' file...'
+
+    samtools sort -n -@ 8 -o $MIDDIR/${name}.bam $INPUTBAM/${name}.bam ;
+    samtools fixmate -m -@ 8 $MIDDIR/${name}.bam $MIDDIR/${name}_fixmate.bam
+    samtools sort -@ 8 -o $MIDDIR/${name}_sorted.bam $MIDDIR/${name}_fixmate.bam
+
+    echo '[INFO] removing duplicates from ' $SAMPLE ' file...'
+
+    #samtools rmdup $MIDDIR/${name}_sorted.bam $INPUTBAM/${name}_RD.bam ;
+    samtools markdup -r -@ 8 $MIDDIR/${name}_sorted.bam $INPUTBAM/${name}_RD.bam ;
+
+    rm $MIDDIR/* ;
+
+    /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" \
+    MarkDuplicatesSpark --input $INPUTBAM/${name}_RD.bam --output $OUTPUTDIR/${name}_001.bam --tmp-dir ./ ;
+
+    echo '[INFO] Marking duplicates in ' $SAMPLE ' file...'
+
+    java -jar /home/dtiezzi/Softwares/picard/build/libs/picard.jar MarkDuplicates \
+    CREATE_INDEX=true \
+    INPUT=$INPUTBAM/${name}_RD.bam \
+    OUTPUT=$OUTPUTDIR/${name}_001.bam \
+    METRICS_FILE=$OUTPUTDIR/${name}_marked_dup_metrics.txt \
+    VALIDATION_STRINGENCY=STRICT
+
+    /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" \
+    BaseRecalibrator -I $OUTPUTDIR/${name}_001.bam -R $REFGENOME \
+    --known-sites snps/common_all_20180418.vcf.gz -O ${name}_recal_data.table ;
+
+    /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" \
+    ApplyBQSR -R $REFGENOME -I $OUTPUTDIR/${name}_001.bam --bqsr-recal-file ${name}_recal_data.table -O $OUTPUTDIR/${name}_md.bam ;
+
+    rm $OUTPUTDIR/${name}_RD.bam $OUTPUTDIR/${name}_001.bam
 
     echo '[INFO] starting analysis on' $name
 
@@ -69,15 +109,13 @@ while read -r line
         chrL=$line1
         chr=$( echo "$chrL" |cut -d\: -f1 )
 
-
-
         /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-Djava.io.tmpdir=/tmp -XX:+UseSerialGC -Xmx3G" Mutect2 \
         -R $REFGENOME \
         -L $chrL -I ./$INPUTBAM/${name}_md.bam \
         -O $VCFFILES/$chr.mt2.vcf \
         -tumor ${name} --af-of-alleles-not-in-resource 2.5e-06 \
         --germline-resource $GNOMAD \
-        --f1r2-tar-gz f1r2.tar.gz \
+        --f1r2-tar-gz ${name}_f1r2.tar.gz \
         -pon $PONFILE
 
     done < 'chrLengths' ;
@@ -88,6 +126,9 @@ while read -r line
     I=input_variant_files.list O=$OUTVCFS/$name.vcf.gz ;
 
     /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" MergeMutectStats --stats input_stats_files.list -O $VCFSORTED/$name.targeted_sequencing.mutect2.tumor_only.sorted.vcf.gz.stats ;
+
+    mv $VCFSORTED/$name.targeted_sequencing.mutect2.tumor_only.sorted.vcf.gz.stats $VCFFINALDIR ;
+    mv $VCFFINALDIR/$name.targeted_sequencing.mutect2.tumor_only.sorted.vcf.gz.stats $VCFFINALDIR/${name}.targeted_sequencing.tumor_only.gatk4_mutect2.raw_somatic_mutation.vcf.gz.stats ; 
 
     ## 6. Sort VCF with Picard
 
@@ -124,6 +165,19 @@ while read -r line
 
     cd ..
 
+    echo '[INFO] Step 9: filtering ' $name 'sample...'
+
+    /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" LearnReadOrientationModel -I f1r2/${name}_f1r2.tar.gz -O ${name}_read-orientation-model.tar.gz
+
+    /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" FilterMutectCalls -V $INPUTDIR/${name}.targeted_sequencing.tumor_only.gatk4_mutect2.raw_somatic_mutation.vcf.gz -R $REFGENOME --ob-priors ${name}_read-orientation-model.tar.gz --min-allele-fraction 0.05 --unique-alt-read-count 10 -O $OUTDIR/${name}_somatic_filtered.vcf
+
+    grep -E "#|PASS" $OUTDIR/${name}_somatic_filtered.vcf > $OUTDIR/${name}_somatic_filtered_PASS.vcf
+
+    /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" Funcotator -R $REFGENOME -V $OUTDIR/${name}_somatic_filtered_PASS.vcf -O $OUTDIR/${name}_somatic_annotated.vcf --output-file-format VCF --data-sources-path $FUNCOTATODATA --ref-version hg38
+
+    /home/dtiezzi/Softwares/gatk-4.1.6.0/gatk --java-options "-XX:+UseSerialGC -Xmx3G" Funcotator -R $REFGENOME -V $OUTDIR/${name}_somatic_filtered_PASS.vcf -O $OUTDIR/${name}_somatic_annotated.maf --output-file-format MAF --data-sources-path $FUNCOTATODATA --ref-version hg38 --annotation-override Tumor_Sample:${name}
+
     echo '[INFO] Analysis DONE in ' $name
+
 
 done < 'bamFiles'
